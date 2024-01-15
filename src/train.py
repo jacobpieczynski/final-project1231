8c2b851aace111089e49872a0106c206efed88a4
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-01-15 16:36:46 -0700

    testing accuracy, without randomness variable

--- src/train.py
+++ src/train.py
@@ -16,31 +16,41 @@ y = data['Home Win']
 #X, y = bc.data, bc.target
 #for data in X:
 #    print(data, end="\n\n")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=705)
+model_average, model_best = 0, 0
+rng = 200
+for i in range(rng):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1) #random_state=705
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-#print(X_train_scaled)
-#model = LogisticRegression()
-#model.fit(X_train_scaled,y_train)
-model = LogisticRegression(lr=0.0001, n_iters=2500)
-model.fit(X_train_scaled,y_train)
-#clf.fit(X_train,y_train)
-#y_pred = clf.predict(X_test)
-y_pred = model.predict(X_test_scaled)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    #print(X_train_scaled)
+    #model = LogisticRegression()
+    #model.fit(X_train_scaled,y_train)
+    model = LogisticRegression(lr=0.0001, n_iters=2500)
+    model.fit(X_train_scaled,y_train)
+    #clf.fit(X_train,y_train)
+    #y_pred = clf.predict(X_test)
+    y_pred = model.predict(X_test_scaled)
 
-def accuracy(y_pred, y_test):
-    return np.sum(y_pred==y_test)/len(y_test)
+    def accuracy(y_pred, y_test):
+        return np.sum(y_pred==y_test)/len(y_test)
 
 
-new_game = pd.read_csv('new_game.csv')
-new_data = new_game.drop('Home Win', axis=1)
-new_data_scaled = scaler.transform(new_data)
-prediction = model.predict(new_data_scaled)
+    new_game = pd.read_csv('new_game.csv')
+    new_data = new_game.drop('Home Win', axis=1)
+    new_data_scaled = scaler.transform(new_data)
+    prediction = model.predict(new_data_scaled)
 
-# Display the prediction
-print("Predicted Outcome:", prediction)
+    # Display the prediction
+    print("Predicted Outcome:", prediction)
 
-acc = accuracy(y_pred, y_test)
-print(acc)
+    acc = accuracy(y_pred, y_test)
+    model_average += acc
+    if acc >= model_best:
+        model_best = acc
+    print(acc)
+
+model_average /= rng
+
+print(f"Overall accuracy is: {model_average}")
